76ceb2da728f9c39cd4d158a618e953e33599afe
Jansielango <149456579+Jansielango@users.noreply.github.com>
2023-10-31 10:57:10 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,28 +1,17 @@
-Challenge 2
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
 
-def sort_students(student_list):
-    # Sort the list of students based on CGPA in descending order
-    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
-    return sorted_students
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-# Test the function
-if __name__ == "__main__":
-    # Create a list of student objects
-    students = [
-        Student("Alice", "S001", 3.9),
-        Student("Bob", "S002", 3.7),
-        Student("Charlie", "S003", 4.0),
-        Student("David", "S004", 3.5),
-    ]
+# Input from the user
+num = int(input("Enter a number: "))
 
-    # Sort the list of students by CGPA
-    sorted_students = sort_students(students)
-
-    # Print the sorted list
-    for student in sorted_students:
-        print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    result = factorial(num)
+    print(f"The factorial of {num} is {result}")
